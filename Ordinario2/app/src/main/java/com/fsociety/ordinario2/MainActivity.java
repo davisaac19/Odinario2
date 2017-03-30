a7f473bb8f475e54dc8e3cc9fd9afa7af71dde19
David May <davisaac8@gmail.com>
2017-03-29 20:24:53 -0600

    Diseño
    Características de diseño.

--- Ordinario2/app/src/main/java/com/fsociety/ordinario2/MainActivity.java
+++ Ordinario2/app/src/main/java/com/fsociety/ordinario2/MainActivity.java
@@ -2,21 +2,28 @@ package com.fsociety.ordinario2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import xyz.hanks.library.SmallBang;
+import xyz.hanks.library.SmallBangListener;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText txtEstatura, txtPeso;
     TextView lblResultado, lblMensaje;
     Button btnCalcular;
+    private SmallBang mSmallBang;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle("Calculadora IMC");
+        mSmallBang = SmallBang.attach2Window(this);
 
         //Referencias a los componentes EditText
         txtEstatura = (EditText) findViewById(R.id.txtAltura);
@@ -28,5 +35,27 @@ public class MainActivity extends AppCompatActivity {
 
         //Referencia al botón
         btnCalcular = (Button) findViewById(R.id.btnCalcular);
+        btnCalcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addNumber(v);
+                //A partir de aquí se hace la operación
+            }
+        });
+    }
+
+    public void addNumber(View view) {
+        mSmallBang.bang(view, new SmallBangListener() {
+            @Override
+            public void onAnimationStart() {
+            }
+
+            @Override
+            public void onAnimationEnd() {
+                Toast.makeText(MainActivity.this, "Botón 1", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
+
+
